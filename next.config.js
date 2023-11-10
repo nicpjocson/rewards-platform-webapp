56fdb8081afec4714523e5793525195f281b5624
howard-rose <philip.anthon@gmail.com>
2023-11-10 21:04:49 +0800

    SSR config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push(
+      "pino-pretty",
+      "lokijs",
+      "encoding"
+    );
+  return config;
+}
+}
 require('dotenv').config();
 
 module.exports = nextConfig
