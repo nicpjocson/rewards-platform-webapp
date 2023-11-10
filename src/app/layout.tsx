1ceb7787dd10e87b1225abd7af5223392fe0a061
howard-rose <philip.anthon@gmail.com>
2023-11-10 21:01:29 +0800

    add web3modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Montserrat } from 'next/font/google'
 import Navbar from '@/components/Navbar/Navbar';
 import Footer from '@/components/Footer/Footer';
 import 'react-toastify/dist/ReactToastify.css';
+import { Web3Modal } from "../context/Web3Modal";
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -21,11 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={montserrat.className}>
-		<Providers>
-          <Navbar />
-		  {children}
-          <Footer />
-		</Providers>
+        <Web3Modal>
+          <Providers>
+            <Navbar />
+            <w3m-button />
+            {children}
+            <Footer />
+          </Providers>
+        </Web3Modal>
         {/* Footer shouldn't be on all pages, but I put it here for testing purposes */}
 	  </body>
     </html>
