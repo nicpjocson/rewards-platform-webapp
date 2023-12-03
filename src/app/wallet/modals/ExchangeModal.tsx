755985cecb640a083ec75267b30b8aac1798f4da
howard-rose <philip.anthon@gmail.com>
2023-12-03 08:57:00 +0800

    exchange token functionality

--- src/app/wallet/modals/ExchangeModal.tsx
+++ src/app/wallet/modals/ExchangeModal.tsx
@@ -11,19 +11,20 @@ import { useForm, Controller } from "react-hook-form";
 import { FormContainer, useFormError } from "@/components/Providers/Forms";
 import Image from "next/image";
 import { useState } from "react";
+import { useContractWrite, useContractRead } from "wagmi";
 
 const ExchangeModal: React.FC<BaseModalProps> = ({ 
     state 
 }) => {
     const tokenPerPoint = 4;
-    const [points, setPoints] = useState(0);
+    const [tokens, setTokens] = useState(0);
     const handleChangePoints = (e: any) => {
         // TODO: handle non-integer input
-        setPoints(e.target.value);
+        setTokens(e.target.value * tokenPerPoint);
     }
     const handleChangeToken = (e: any) => {
         // TODO: convert token to points
-        setPoints(e.target.value / tokenPerPoint);
+        setTokens(e.target.value);
     }
 
     const form = useForm();
@@ -31,16 +32,42 @@ const ExchangeModal: React.FC<BaseModalProps> = ({
     
     const close = () => {
         state[1](false);
-        setPoints(0);
+        setTokens(0);
     }
+
+    const tokensToExchange = tokens.toString();
+    const HLOToken_address = "0xb2bE0F7CC870deEa96eBD115bC8CF81D64bEd9D2";
+    const exchangeContract_address = "0x594f1b32915dEc816ECb1c706A1E466b48e5daFD";
+    const HLOToken_abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"allowance","type":"uint256"},{"internalType":"uint256","name":"needed","type":"uint256"}],"name":"ERC20InsufficientAllowance","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"uint256","name":"balance","type":"uint256"},{"internalType":"uint256","name":"needed","type":"uint256"}],"name":"ERC20InsufficientBalance","type":"error"},{"inputs":[{"internalType":"address","name":"approver","type":"address"}],"name":"ERC20InvalidApprover","type":"error"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"}],"name":"ERC20InvalidReceiver","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"}],"name":"ERC20InvalidSender","type":"error"},{"inputs":[{"internalType":"address","name":"spender","type":"address"}],"name":"ERC20InvalidSpender","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}];
+    const exchangeContract_abi = [{"inputs":[{"internalType":"address","name":"_customToken","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"TokensExchanged","type":"event"},{"inputs":[],"name":"customToken","outputs":[{"internalType":"contract HelloToken","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"exchangeTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_newOwnerAddress","type":"address"}],"name":"setOwner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"tokens","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdrawTokens","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+
+    const { data, isLoading, isSuccess, write } = useContractWrite({
+        address: HLOToken_address,
+        abi: HLOToken_abi,
+        functionName: 'approve',
+        args: [exchangeContract_address, tokensToExchange],
+    })
+    const { data: dataExchange, isLoading: isLoadingExchange, isSuccess: isSuccessExchange, write: writeExchange } = useContractWrite({
+        address: exchangeContract_address,
+        abi: exchangeContract_abi,
+        functionName: 'exchangeTokens',
+        args: [tokensToExchange],
+    })
     
-	const handleSubmit = form.handleSubmit((data) => {
-        console.log('Exchanged');
+    const handleApprove = () => {
+        console.log(tokensToExchange);
+        write();
         console.log(data);
-        console.log(points);
+    }
+
+	const handleSwap = form.handleSubmit(() => {
+        console.log(tokensToExchange);
+        writeExchange();
+        console.log(dataExchange);
+        console.log('Exchanged');
 		close();
 		form.reset();
-	});
+    });
 
     return (
         <BaseModal state={state}>
@@ -53,10 +80,10 @@ const ExchangeModal: React.FC<BaseModalProps> = ({
                         <Image src="/icons/gift.svg" width={122} height={122} alt="Reward Points" />
                     </CardRow>
                     <div>
-                        <ExchangeInput id="token" title="YOU PAY" label="Token" labelClassName={styles.token} convertedValue={1000} value={points * tokenPerPoint} onChange={handleChangeToken} />
+                        <ExchangeInput id="token" title="YOU PAY" label="Token" labelClassName={styles.token} convertedValue={1000} value={tokens} onChange={handleChangeToken} />
                         <div className={styles.text_info}>Available: P{123456.78}</div>
                         <br />
-                        <ExchangeInput id="points" title="YOU RECEIVE" label="Points" labelClassName={styles.points} convertedValue={950} value={points} onChange={handleChangePoints} />
+                        <ExchangeInput id="points" title="YOU RECEIVE" label="Points" labelClassName={styles.points} convertedValue={950} value={tokens / tokenPerPoint} onChange={handleChangePoints} />
                         <br /><br />
                         <div className={`${styles.gas_fee_row} ${styles.text_info}`}>
                             <div className={styles.gas_fee_label}>Estimated gas fee:</div> 
@@ -65,7 +92,10 @@ const ExchangeModal: React.FC<BaseModalProps> = ({
                         <br />
                     </div>
                     <CardRow>
-                        <SideButton onClick={handleSubmit} color="yellow">
+                        <SideButton onClick={handleApprove}>
+                            Approve
+                        </SideButton>
+                        <SideButton onClick={handleSwap} color="yellow">
                             Swap
                         </SideButton>
                         <SideButton onClick={close} color="gray">
